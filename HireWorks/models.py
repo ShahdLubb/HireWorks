2ba9374d37c393582beed74dba3fc1384781b833
Shahd <shahdlubb@gmail.com>
2023-05-19 17:30:42 +0300

    database models were added

--- HireWorks/models.py
+++ HireWorks/models.py
@@ -2,20 +2,59 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    age = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+class Employer(models.Model):
+    employer_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
 
     class Meta:
+        db_table = 'employers'
         app_label = 'HireWorks'
-        db_table = 'HireWorks.person'
-        
+    using = 'HireWorks'
 
+
+class Job(models.Model):
+    job_id = models.IntegerField(primary_key=True)
+    employer_id = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    salary = models.IntegerField()
+    requirements = models.TextField()
+    location = models.CharField(max_length=20)
+    deadline = models.DateField()
+
+    class Meta:
+        db_table = 'jobs'
+        app_label = 'HireWorks'
+    using = 'HireWorks'
+
+
+class Candidate(models.Model):
+    candidate_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'candidates'
+        app_label = 'HireWorks'
+    using = 'HireWorks'
+
+
+class Application(models.Model):
+    application_id = models.IntegerField(primary_key=True)
+    resume = models.CharField(max_length=50)
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+    candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('job_id', 'candidate_id',)
+        db_table = 'applications'
+        app_label = 'HireWorks'
     using = 'HireWorks'
 
    
