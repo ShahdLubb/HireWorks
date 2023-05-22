add36bf91d44407415b0e794092140adb85798b4
Shahd <shahdlubb@gmail.com>
2023-05-22 04:43:12 +0300

    implement check on job application deadline

--- HireWorks/views.py
+++ HireWorks/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-# Create your views here.
-
-
+import datetime
 import json
 from django.http import HttpResponse, JsonResponse
 from django.views.defaults import page_not_found
@@ -52,10 +50,17 @@ def submit_job_application(request):
             job_id = data.get('job_id')
             email = data.get('email')
             resume = data.get('resume')
-
-            # create the application object
+            
+            # get the job and candidate who is applying
             job = Job.objects.using("HireWorks").get(job_id=job_id)
             candidate = Candidate.objects.using("HireWorks").get(email=email)
+
+            #check the job deadline
+            CurrentDate = datetime.date.today()
+            if(job.deadline < CurrentDate):
+                response_data = {'success': False, 'message': ' job application deadline has already passed'}
+                return JsonResponse(response_data)
+            # create the application object
             next_application_id = Application.objects.using("HireWorks").all().order_by('-application_id').first().application_id + 1
             application = Application(application_id=next_application_id,resume=resume, job=job, candidate=candidate)
 
