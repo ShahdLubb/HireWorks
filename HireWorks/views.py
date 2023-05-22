7415256231b0b961a22a5ddc29567a5aa87a4042
Shahd <shahdlubb@gmail.com>
2023-05-22 05:47:50 +0300

    impleminting exchange functionality using external API

--- HireWorks/views.py
+++ HireWorks/views.py
@@ -6,39 +6,56 @@ from django.http import HttpResponse, JsonResponse
 from django.views.defaults import page_not_found
 from HireWorks.models import Application, Candidate, Job
 from HireWorks.models import Employer
+from HireWorks.exchange import exchange_amount
 
 def job_list(request):
     if request.method == 'GET':
         jobs = Job.objects.using('HireWorks').all()
-        return jobs_details(jobs)
+        currency="USD"
+        if 'currency' in request.GET:
+            currency = request.GET['currency']
+            print(currency)
+        return jobs_details(jobs,currency)
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
 def search_jobs_by_location(request, location):
     if request.method == 'GET':
         jobs = Job.objects.using('HireWorks').filter(location=location)
-        return jobs_details(jobs)
+        currency="USD"
+        if 'currency' in request.GET:
+            currency = request.GET['currency']
+        return jobs_details(jobs,currency)
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
 def search_jobs_by_title(request, title):
     if request.method == 'GET':
         jobs = Job.objects.using('HireWorks').filter(title=title)
-        return jobs_details(jobs)
+        currency="USD"
+        if 'currency' in request.GET:
+            currency = request.GET['currency']
+        return jobs_details(jobs,currency)
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
 def search_jobs_by_salary(request, salary):
     if request.method == 'GET':
         jobs = Job.objects.using('HireWorks').filter(salary__gte=salary)
-        return jobs_details(jobs)
+        currency="USD"
+        if 'currency' in request.GET:
+            currency = request.GET['currency']
+        return jobs_details(jobs,currency)
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
 def search_jobs_by_salary_range(request, min_salary,max_salary):
     if request.method == 'GET':
         jobs = Job.objects.using('HireWorks').filter(salary__gte=min_salary,salary__lte=max_salary)
-        return jobs_details(jobs)
+        currency="USD"
+        if 'currency' in request.GET:
+            currency = request.GET['currency']
+        return jobs_details(jobs,currency)
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
@@ -77,13 +94,13 @@ def submit_job_application(request):
     else:
         return page_not_found(request, exception=Exception('Page not Found'))
 
-def jobs_details(jobs):
+def jobs_details(jobs,currency):
     job_data = [
             {
                 'company name':job.employer.company_name,
                 'title': job.title,
                 'description': job.description,
-                'salary':"{}$".format(job.salary),
+                'salary':format_salary(job.salary,currency),
                 'requirments':job.requirements,
                 'location': job.location,
                 'application deadline': job.deadline.strftime('%Y-%m-%d')
@@ -95,3 +112,12 @@ def jobs_details(jobs):
     json_data = json.dumps(job_data, indent=4)
     return HttpResponse(json_data, content_type='application/json')
 
+def format_salary(salary,currency):
+    if currency != "USD":
+        NewSalary=exchange_amount(currency,salary)
+        if NewSalary == None:
+            currency="USD"
+        else: 
+           salary=NewSalary
+
+    return "{} {}".format(salary,currency)
