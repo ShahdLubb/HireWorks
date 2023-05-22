bfadf6eaaeb2754458928e8a179c87a0a5c982a9
Shahd <shahdlubb@gmail.com>
2023-05-22 21:32:06 +0300

    add seperate file for submit_application

---
+++ HireWorks/submit_application.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import datetime
+import json
+from django.http import HttpResponse, JsonResponse
+from django.views.defaults import page_not_found
+from HireWorks.models import Application, Candidate, Job
+from HireWorks.models import Employer
+from HireWorks.exchange import exchange_amount
+from HireWorks.job_utility import *
+
+@csrf_exempt
+def submit_job_application(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            job_id = data.get('job_id')
+            email = data.get('email')
+            resume = data.get('resume')
+            
+            # get the job and candidate who is applying
+            job = Job.objects.using("HireWorks").get(job_id=job_id)
+            candidate = Candidate.objects.using("HireWorks").get(email=email)
+
+            #check the job deadline
+            CurrentDate = datetime.date.today()
+            if(job.deadline < CurrentDate):
+                response_data = {'success': False, 'message': ' job application deadline has already passed'}
+                return JsonResponse(response_data)
+            # create the application object
+            next_application_id = Application.objects.using("HireWorks").all().order_by('-application_id').first().application_id + 1
+            application = Application(application_id=next_application_id,resume=resume, job=job, candidate=candidate)
+
+            # save the application to the database
+            application.save(using='HireWorks')
+
+            # return a success response
+            response_data = {'success': True, 'message': 'Application submitted successfully'}
+            return JsonResponse(response_data)
+        except Exception as e:
+            # return an error response if any exception occurs
+            response_data = {'success': False, 'message': str(e)}
+            return JsonResponse(response_data, status=400)
+    else:
+        return page_not_found(request, exception=Exception('Page not Found'))
+
+
